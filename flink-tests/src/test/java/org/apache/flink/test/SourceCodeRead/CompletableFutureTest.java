60abdceb6356a93394992626e10c77bb0e6b332a
loushang <loushang@vipkid.com.cn>
2019-10-31 21:14:17 +0800

    complete

--- flink-tests/src/test/java/org/apache/flink/test/SourceCodeRead/CompletableFutureTest.java
+++ flink-tests/src/test/java/org/apache/flink/test/SourceCodeRead/CompletableFutureTest.java
@@ -1,18 +1,19 @@
 package org.apache.flink.test.SourceCodeRead;
 
+import org.apache.flink.shaded.netty4.io.netty.util.concurrent.CompleteFuture;
 import org.junit.Test;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.*;
 
 public class CompletableFutureTest {
 
-	class Student{
+	class Student {
 		int id;
 		String name;
 
-		public Student(int id,String name){
+		public Student(int id, String name) {
 			this.id = id;
 			this.name = name;
 		}
@@ -31,22 +32,22 @@ public class CompletableFutureTest {
 	}
 
 	@Test
-	public void test1(){
+	public void test1() {
 
-		Student student = new Student(1,"li");
-		String[] list = {"a","b","c"};
+		Student student = new Student(1, "li");
+		String[] list = {"a", "b", "c"};
 
 		List<CompletableFuture<String>> resList = new ArrayList<>();
 
-		for(String str: list){
-			resList.add(CompletableFuture.supplyAsync(()->str).thenApply(e->e.toUpperCase()) );
+		for (String str : list) {
+			resList.add(CompletableFuture.supplyAsync(() -> str).thenApply(e -> e.toUpperCase()));
 		}
 
 		CompletableFuture.allOf(resList.toArray(new CompletableFuture[resList.size()]))
-			.whenComplete((r,e) -> {
-				if(e == null){
+			.whenComplete((r, e) -> {
+				if (e == null) {
 					student.setName("zhu");
-				}else{
+				} else {
 					throw new RuntimeException(e);
 				}
 			});
@@ -55,23 +56,43 @@ public class CompletableFutureTest {
 	}
 
 	@Test
-	public void test2(){
+	public void test2() {
 
-		String result = CompletableFuture.supplyAsync(()->{
+		String result = CompletableFuture.supplyAsync(() -> {
 			try {
 				Thread.sleep(100);
 			} catch (InterruptedException e) {
 				e.printStackTrace();
 			}
-			if(true) {
+			if (true) {
 				throw new RuntimeException("exception test!");
 			}
 
 			return "Hi Boy";
-		}).exceptionally(e->{
+		}).exceptionally(e -> {
 			System.out.println(e.getMessage());
 			return "Hello world!";
-		}).join(); 		//这种一个的join之后返回结果
+		}).join();        //这种一个的join之后返回结果
 		System.out.println(result);
 	}
+
+	//基本使用
+	@Test
+	public void test3() {
+		CompletableFuture<String> completableFuture = new CompletableFuture<>();
+
+		new Thread(() -> {
+			completableFuture.complete(Thread.currentThread().getName());
+		}).start();
+
+		//doSomethingElse
+
+		try {
+			System.out.println(completableFuture.get());
+		}catch (Exception e){
+			e.printStackTrace();
+		}
+	}
+
+
 }
